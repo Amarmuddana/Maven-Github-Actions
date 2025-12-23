c907ccede37cb92f274f6b2f2fc9e1c7b397d155
amar <amar.muddana@gmail.com>
2025-12-23 12:42:30 -0500

    1st commint git hub actions

---
+++ helloworld-war/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World JSP Page</h2>
+<p>Go to <a href="hello">/hello</a> to see the servlet.</p>
+</body>
+</html>
---
+++ helloworld-war/src/test/java/com/example/helloworld/HelloServletTest.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloServletTest {
+
+    @Test
+    public void exampleTest() {
+        assertEquals(4, 2 + 2, "Basic math test");
+    }
+}
